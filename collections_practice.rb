9522b1db4b7729ee23334e5a44d7a31aa9afcf40
Adam Clemons <74882592+thisClemons@users.noreply.github.com>
2021-10-07 14:14:29 -0400

    commit tree d304f1297457b3022d54a9e519e5304ef2ae2c77

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,65 @@
+require 'pry'
 
+def sort_array_asc(arr)
+    arr.sort    
+end
+
+def sort_array_desc(arr)
+    arr.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(arr) 
+    arr.sort do |a, b|
+        a.length <=> b.length        
+    end
+end
+
+def swap_elements(arr)
+    arr[1], arr[2] = arr[2], arr[1]
+    arr
+end
+
+def reverse_array(arr)
+    arr.reverse     
+end
+
+def kesha_maker(arr)
+    keshed = []
+    arr.each do |word|
+        word[2] = "$"
+        keshed << word       
+    end
+    keshed
+end
+
+def find_a(arr)
+    a_words = []
+    arr.each {|word| a_words << word if word[0] == "a"}
+    a_words
+end
+
+def sum_array(arr)
+    sum = 0
+    arr.each do |num|
+        sum += num
+    end
+    sum
+end
+
+def add_s(arr)
+    # arr.collect do |word|
+    #     if arr[1] == word
+    #         word
+    #     else
+    #         word + "s"
+    #     end
+    # end
+
+    arr.each_with_index.collect do |element, index|
+        index == 1 ? element : element + "s"
+    end
+
+
+end
+
+# binding.pry
